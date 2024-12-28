2c2ee588f5f7cad4817fdb06daf7cb03e60169d7
sihuai2412 <sihuai2022@163.com>
2024-12-28 13:22:34 +0800

    add test project

---
+++ build.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+dotnet build
+dotnet test .\epsilon.Tests\epsilon.Tests.csproj
--- epsilon/obj/Debug/net9.0/epsilon.AssemblyInfo.cs
+++ epsilon/obj/Debug/net9.0/epsilon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a39a31da1ef3db5a521cd533418e72994d4ecd6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+410f4f84d7b82a4dbfa3c219842a7191bb835f8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
