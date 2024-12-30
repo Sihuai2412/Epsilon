56170f2d5637aa7c74b71dca99cf8ff0c1eabbd0
sihuai2412 <sihuai2022@163.com>
2024-12-30 20:12:17 +0800

    move TextSpan to Text namespace

--- epsilon.Tests/obj/Debug/net9.0/epsilon.Tests.AssemblyInfo.cs
+++ epsilon.Tests/obj/Debug/net9.0/epsilon.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("epsilon.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6779a3ad200ed36eb28762886bfc05adb20b8ee0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c929e0670f873c1c04ff92d76c6c70f9bda61050")]
 [assembly: System.Reflection.AssemblyProductAttribute("epsilon.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("epsilon.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- epsilon/obj/Debug/net9.0/epsilon.AssemblyInfo.cs
+++ epsilon/obj/Debug/net9.0/epsilon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f32bda9ed4be2a911e85e9de316f575136d1564")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c929e0670f873c1c04ff92d76c6c70f9bda61050")]
 [assembly: System.Reflection.AssemblyProductAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
