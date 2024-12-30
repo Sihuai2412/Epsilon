9d5df1c8f9e713e73f822a720ffd5003e94e3758
sihuai2412 <sihuai2022@163.com>
2024-12-30 18:24:16 +0800

    clean-up parser

--- epsilon/obj/Debug/net9.0/epsilon.AssemblyInfo.cs
+++ epsilon/obj/Debug/net9.0/epsilon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d695b7999cb2432683c33c2b06b7d2094840a14f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f05de66abec61097b1b37d8ddb309c3387db874")]
 [assembly: System.Reflection.AssemblyProductAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
