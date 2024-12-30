263aa861807fec0044f853acb569fd2375d2baad
sihuai2412 <sihuai2022@163.com>
2024-12-30 21:24:03 +0800

    fix null reference for cases where the token was inserted

--- epsilon/CodeAnalysis/Text/SourceText.cs
+++ epsilon/CodeAnalysis/Text/SourceText.cs
@@ -18,22 +18,16 @@ public sealed class SourceText {
     public int GetLineIndex(int position){
         var lower = 0;
         var upper = Lines.Length - 1;
-
         while (lower <= upper){
             var index = lower + (upper - lower) / 2;
             var start = Lines[index].Start;
-
-            if (position == start){
-                return index;
-            }
-
+            if (position == start) return index;
             if (start > position){
                 upper = index - 1;
             } else {
-                lower = index - 1;
+                lower = index + 1;
             }
         }
-
         return lower - 1;
     }
 
--- epsilon/obj/Debug/net9.0/epsilon.AssemblyInfo.cs
+++ epsilon/obj/Debug/net9.0/epsilon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56170f2d5637aa7c74b71dca99cf8ff0c1eabbd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdeeed0cb9c0f8b5df90434149bf1a2a3db32ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
