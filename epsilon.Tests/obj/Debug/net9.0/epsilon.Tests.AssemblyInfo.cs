7bd33da6e330b319cd17a1f6e95f19f07febda0c
sihuai2412 <sihuai2022@163.com>
2024-12-31 17:45:43 +0800

    add support for multiple lines in REPL

--- epsilon.Tests/obj/Debug/net9.0/epsilon.Tests.AssemblyInfo.cs
+++ epsilon.Tests/obj/Debug/net9.0/epsilon.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("epsilon.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56170f2d5637aa7c74b71dca99cf8ff0c1eabbd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263aa861807fec0044f853acb569fd2375d2baad")]
 [assembly: System.Reflection.AssemblyProductAttribute("epsilon.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("epsilon.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- epsilon/obj/Debug/net9.0/epsilon.AssemblyInfo.cs
+++ epsilon/obj/Debug/net9.0/epsilon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdeeed0cb9c0f8b5df90434149bf1a2a3db32ed6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263aa861807fec0044f853acb569fd2375d2baad")]
 [assembly: System.Reflection.AssemblyProductAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("epsilon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
