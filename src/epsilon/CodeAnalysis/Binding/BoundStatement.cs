7c1b2a0e295983041098adf2098f39a6b41bb81e
Sihuai2412 <sihuai2022@163.com>
2025-01-10 19:54:01 +0800

    move BoundForStatement and BoundWhileStatement to new file

--- src/epsilon/CodeAnalysis/Binding/BoundStatement.cs
+++ src/epsilon/CodeAnalysis/Binding/BoundStatement.cs
@@ -3,18 +3,3 @@ namespace epsilon.CodeAnalysis.Binding;
 internal abstract class BoundStatement : BoundNode {
 
 }
-
-internal sealed class BoundForStatement : BoundStatement {
-    public BoundForStatement(VariableSymbol variable, BoundExpression lowerBound, BoundExpression upperBound, BoundStatement body){
-        Variable = variable;
-        LowerBound = lowerBound;
-        UpperBound = upperBound;
-        Body = body;
-    }
-
-    public override BoundNodeKind Kind => BoundNodeKind.ForStatement;
-    public VariableSymbol Variable { get; }
-    public BoundExpression LowerBound { get; }
-    public BoundExpression UpperBound { get; }
-    public BoundStatement Body { get; }
-}
