2d3952b39338ae2df58a027662842528a85c6509
sihuai2412 <sihuai2022@163.com>
2025-01-12 16:18:38 +0800

    add support for bitwise operators

--- src/epsilon.Program/obj/Debug/net9.0/epsilon.Program.AssemblyInfo.cs
+++ src/epsilon.Program/obj/Debug/net9.0/epsilon.Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("epsilon.Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f210cf2dc381ae34e84d550e78e08550f990f47c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28965cfb1d3545926065282019c1b3b94cf588f")]
 [assembly: System.Reflection.AssemblyProductAttribute("epsilon.Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("epsilon.Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
