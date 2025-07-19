1f522fac82c85f5e8838dd87bfe3e337ad953cbd
sihuai2412 <sihuai2022@163.com>
2025-07-19 19:20:47 +0800

    emit binary expressions

--- src/epsilon/CodeAnalysis/Emit/Emitter.cs
+++ src/epsilon/CodeAnalysis/Emit/Emitter.cs
@@ -15,6 +15,7 @@ internal sealed class Emitter {
     private readonly Dictionary<FunctionSymbol, MethodDefinition> _methods = new Dictionary<FunctionSymbol, MethodDefinition>();
     private readonly AssemblyDefinition _assemblyDefinition;
     private readonly Dictionary<TypeSymbol, TypeReference> _knownTypes;
+    private readonly MethodReference _objectEqualsReference;
     private readonly MethodReference _consoleWriteLineReference;
     private readonly MethodReference _consoleReadLineReference;
     private readonly MethodReference _stringConcatReference;
@@ -113,6 +114,7 @@ internal sealed class Emitter {
             return null;
         }
 
+        _objectEqualsReference = ResolveMethod("System.Object", "Equals", ["System.Object", "System.Object"]);
         _consoleWriteLineReference = ResolveMethod("System.Console", "WriteLine", ["System.String"]);
         _consoleReadLineReference = ResolveMethod("System.Console", "ReadLine", Array.Empty<string>());
         _stringConcatReference = ResolveMethod("System.String", "Concat", ["System.String", "System.String"]);
@@ -340,17 +342,99 @@ internal sealed class Emitter {
     }
 
     private void EmitBinaryExpression(ILProcessor ilProcessor, BoundBinaryExpression node) {
+        EmitExpression(ilProcessor, node.Left);
+        EmitExpression(ilProcessor, node.Right);
+
         if (node.Op.Kind == BoundBinaryOperatorKind.Addition) {
             if (node.Left.Type == TypeSymbol.String &&
-                 node.Right.Type == TypeSymbol.String) {
-                EmitExpression(ilProcessor, node.Left);
-                EmitExpression(ilProcessor, node.Right);
+                node.Right.Type == TypeSymbol.String) {
                 ilProcessor.Emit(OpCodes.Call, _stringConcatReference);
-            } else {
-                throw new NotImplementedException();
+                return;
             }
-        } else {
-            throw new NotImplementedException();
+        }
+
+        if (node.Op.Kind == BoundBinaryOperatorKind.Equals) {
+            if (node.Left.Type == TypeSymbol.Any && node.Right.Type == TypeSymbol.Any ||
+                node.Left.Type == TypeSymbol.String && node.Right.Type == TypeSymbol.String) {
+                ilProcessor.Emit(OpCodes.Call, _objectEqualsReference);
+                return;
+            }
+        }
+
+        if (node.Op.Kind == BoundBinaryOperatorKind.NotEquals) {
+            if (node.Left.Type == TypeSymbol.Any && node.Right.Type == TypeSymbol.Any ||
+                node.Left.Type == TypeSymbol.String && node.Right.Type == TypeSymbol.String) {
+                ilProcessor.Emit(OpCodes.Call, _objectEqualsReference);
+                ilProcessor.Emit(OpCodes.Ldc_I4_0);
+                ilProcessor.Emit(OpCodes.Ceq);
+                return;
+            }
+        }
+
+        switch (node.Op.Kind) {
+            case BoundBinaryOperatorKind.Addition: {
+                    ilProcessor.Emit(OpCodes.Add);
+                    break;
+                }
+            case BoundBinaryOperatorKind.Subtraction: {
+                    ilProcessor.Emit(OpCodes.Sub);
+                    break;
+                }
+            case BoundBinaryOperatorKind.Multiplication: {
+                    ilProcessor.Emit(OpCodes.Mul);
+                    break;
+                }
+            case BoundBinaryOperatorKind.Division: {
+                    ilProcessor.Emit(OpCodes.Div);
+                    break;
+                }
+            case BoundBinaryOperatorKind.BitwiseAnd:
+            case BoundBinaryOperatorKind.LogicalAnd: {
+                    ilProcessor.Emit(OpCodes.And);
+                    break;
+                }
+            case BoundBinaryOperatorKind.BitwiseOr:
+            case BoundBinaryOperatorKind.LogicalOr: {
+                    ilProcessor.Emit(OpCodes.Or);
+                    break;
+                }
+            case BoundBinaryOperatorKind.BitwiseXOr: {
+                    ilProcessor.Emit(OpCodes.Xor);
+                    break;
+                }
+            case BoundBinaryOperatorKind.Equals: {
+                    ilProcessor.Emit(OpCodes.Ceq);
+                    break;
+                }
+            case BoundBinaryOperatorKind.NotEquals: {
+                    ilProcessor.Emit(OpCodes.Ceq);
+                    ilProcessor.Emit(OpCodes.Ldc_I4_0);
+                    ilProcessor.Emit(OpCodes.Ceq);
+                    break;
+                }
+            case BoundBinaryOperatorKind.Less: {
+                    ilProcessor.Emit(OpCodes.Clt);
+                    break;
+                }
+            case BoundBinaryOperatorKind.LessOrEquals: {
+                    ilProcessor.Emit(OpCodes.Cgt);
+                    ilProcessor.Emit(OpCodes.Ldc_I4_0);
+                    ilProcessor.Emit(OpCodes.Ceq);
+                    break;
+                }
+            case BoundBinaryOperatorKind.Greater: {
+                    ilProcessor.Emit(OpCodes.Cgt);
+                    break;
+                }
+            case BoundBinaryOperatorKind.GreaterOrEquals: {
+                    ilProcessor.Emit(OpCodes.Clt);
+                    ilProcessor.Emit(OpCodes.Ldc_I4_0);
+                    ilProcessor.Emit(OpCodes.Ceq);
+                    break;
+                }
+            default: {
+                    throw new Exception($"Unexpected binary operator {SyntaxFacts.GetText(node.Op.SyntaxKind)}({node.Left.Type}, {node.Right.Type})");
+                }
         }
     }
 
